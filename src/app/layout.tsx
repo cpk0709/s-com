3846f500a069c3b9983508ab3cdd33c7a5cdf5de
phil-0709 <126749898+phil-0709@users.noreply.github.com>
2024-10-08 14:32:23 +0900

    home page ui 구조

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>Root Layout</h1>
+        {/* <h1>Root Layout</h1> */}
         {children}
       </body>
     </html>
